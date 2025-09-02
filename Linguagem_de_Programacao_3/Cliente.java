1c5a58a67b07787ca032656650025fa063176491
Ana Paula <166036823+AnaPaulaSPin@users.noreply.github.com>
2025-09-02 19:34:29 -0300

    Servidor e Cliente
    Aprendendo sobre comunicação entre um servidor e rede

--- Linguagem_de_Programacao_3/Cliente.java
+++ Linguagem_de_Programacao_3/Cliente.java
@@ -1,5 +1,3 @@
-package com.mycompany.primeira_aula;
-
 import java.io.ObjectInputStream;
 import java.net.Socket;
 import java.util.Date;
---
+++ Linguagem_de_Programacao_3/Servidor.java
@@ -0,0 +1,29 @@
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Date;
+
+public class Servidor {
+   public static void main(String[] args){
+        try {
+            ServerSocket servidor = new ServerSocket(1234);
+            System.out.println("Servidor ouvindo a porta 1234...");
+            while (true) { 
+                Socket cliente = servidor.accept();
+                System.out.println("Cliente conectado: " + cliente.getInetAddress().getHostAddress());
+
+                ObjectOutputStream saida = new ObjectOutputStream(cliente.getOutputStream());
+                saida.flush();
+                saida.writeObject(new Date());
+                saida.close();
+                cliente.close();
+            }
+        } catch (Exception e){
+            System.out.println("Erro: " + e.getMessage());
+        } 
+         finally{ 
+
+         }
+
+        }
+}
