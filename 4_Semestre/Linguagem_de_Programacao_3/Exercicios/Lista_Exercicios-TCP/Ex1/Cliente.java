df47cdce4125f02089cc1a83c0e834e8354d562d
Ana Paula <166036823+AnaPaulaSPin@users.noreply.github.com>
2025-10-11 09:55:48 -0300

    Conexão TCP - Revisão
    Implementando um servidor TCP que aceita conexões de clientes e ecoa de volta qualquer mensagem recebida.

--- 4_Semestre/Linguagem_de_Programacao_3/Exercicios/Lista_Exercicios-TCP/Ex1/Cliente.java
+++ 4_Semestre/Linguagem_de_Programacao_3/Exercicios/Lista_Exercicios-TCP/Ex1/Cliente.java
@@ -1,5 +1,24 @@
+import java.io.ObjectInputStream;
+import java.net.Socket;
+
+import javax.swing.JOptionPane;
+
 public class Cliente {
     public static void main(String[] args) {
-        
+       try {
+         Socket cliente = new Socket("localhost", 1234);
+         ObjectInputStream entrada = new ObjectInputStream(cliente.getInputStream());
+
+         String mensagem = (String) entrada.readObject();
+
+         JOptionPane.showMessageDialog(null, "Mensagem recebida: " + mensagem);
+
+         cliente.close();
+         entrada.close();
+
+       } catch (Exception e) {
+         e.printStackTrace();
+
+       }
     }
 }
--- 4_Semestre/Linguagem_de_Programacao_3/Exercicios/Lista_Exercicios-TCP/Ex1/Servidor.java
+++ 4_Semestre/Linguagem_de_Programacao_3/Exercicios/Lista_Exercicios-TCP/Ex1/Servidor.java
@@ -1,5 +1,23 @@
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Servidor {
     public static void main(String[] args) {
-        
+        try {
+            ServerSocket servidor = new ServerSocket(1234);
+            while(true){
+                Socket cliente = servidor.accept();
+                ObjectOutputStream saida = new ObjectOutputStream(cliente.getOutputStream());
+                saida.flush();
+                saida.writeObject("Olaaaa, vc foi conectado aoo servidor 1234 com sucesso!");
+
+                saida.close();
+                cliente.close();
+            }
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
